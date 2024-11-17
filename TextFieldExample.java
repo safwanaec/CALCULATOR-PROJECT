04aef87afbba8fe7a8ae167cd9a6d336d459a080
Devit Chandra Roy <devitroy15@gmail.com>
2024-11-17 13:17:30 +0600

    another textfield is created

--- TextFieldExample.java
+++ TextFieldExample.java
@@ -1,6 +1,7 @@
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
+
 class TextFieldExample    
 {    
     public static void main(String args[])    
@@ -8,7 +9,7 @@ class TextFieldExample
         // Creating a JFrame object with title "TextField Example."  
         JFrame f= new JFrame("TextField Example");    
         // Creating two JTextField objects  
-        JTextField t1, t2;    
+        JTextField t1, t2,t3;    
         // Initializing the first JTextField with default text "Welcome to Javatpoint."  
         t1 = new JTextField("Welcome to Javatpoint.");    
         // Setting the position and size of the first JTextField  
@@ -17,13 +18,19 @@ class TextFieldExample
         t2 = new JTextField("AWT Tutorial");    
         // Setting the position and size of the second JTextField  
         t2.setBounds(50,150, 200,30);    
+
+        t3 = new JTextField("Get ready to learn");
+        t3.setBounds(50,200, 200,30);
+
         // Adding JTextFields to the JFrame  
         f.add(t1);  
         f.add(t2);  
+        f.add(t3);
         // Setting the size of the JFrame  
         f.setSize(400,400);    
         // Setting layout to null to use absolute positioning  
         f.setLayout(null);    
+       
         // Making the JFrame visible  
         f.setVisible(true);    
     }    
